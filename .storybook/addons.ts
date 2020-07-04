e479ca4a2a17989b4b81f9d4e4fe4eb657c5c1ba
fhx10012091 <1875934025@qq.com>
2020-07-04 19:34:16 +0800

    upload to github pages

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -1,3 +1,5 @@
 import '@storybook/addons'
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+// github上面生成的token令牌
+// 129b1bceb7fb71fc5acf1129e1463502c4f4eda6
---
+++ storybook-static/precache-manifest.06eb2a6a15eace1fb564f14f23dedd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d467a9cb4e06fcce14a8e4792fef48ef",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d145cfef0c3b1619c3ee.bundle.js"
+  },
+  {
+    "url": "runtime~main.d145cfef0c3b1619c3ee.bundle.js"
+  },
+  {
+    "revision": "1a94d4e149139a85e6c7",
+    "url": "static/css/main.315fd4a6.chunk.css"
+  },
+  {
+    "revision": "9ff4e6cda89b52b672d4",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.d145cfef0c3b1619c3ee.bundle.js"
+  },
+  {
+    "url": "vendors~main.d145cfef0c3b1619c3ee.bundle.js.LICENSE.txt"
+  }
+]);
